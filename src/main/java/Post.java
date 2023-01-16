a7bb4fe3c995adbf692929cb4a1dc52cf29f1e3e
Viktor <ren1399@yandex.ru>
2023-01-16 17:32:29 +0500

    first commit

---
+++ src/main/java/Post.java
@@ -0,0 +1,9 @@
+public class Post {
+    public FormDate birthday;
+    public String name; // Поле для имени
+    public String passport; // Номер паспорта
+    public String patronymic; // Отчество
+    public String phone; // Номер телефона
+    public String surname; // Фамилия
+    public boolean subscription; // Подписка
+}
